93f6b67a193ae55083a57e7442a71fd2e24be4a5
Anton H <anton.g@ksyste.ms>
2022-03-22 20:09:06 +0100

    test

--- Millionaire/UI/MainMenuViewController.swift
+++ Millionaire/UI/MainMenuViewController.swift
@@ -20,7 +20,6 @@ class MainMenuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
